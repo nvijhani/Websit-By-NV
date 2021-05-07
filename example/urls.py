dab74901d0b3ab081a5578eeebeaf3b572300416
Nakul <nakulvijhani123@gmail.com>
2021-05-07 16:11:24 +0530

    Added last changes

--- example/urls.py
+++ example/urls.py
@@ -1,4 +1,4 @@
-"""django_project URL Configuration
+"""Hello URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from example import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('example.urls'))
+    path("", views.index, name='home'),
+    path("about", views.about, name='about'),
+    path("projects", views.projects, name='project'),
+    path("suggestions", views.suggestions, name='suggestions')
 ]
